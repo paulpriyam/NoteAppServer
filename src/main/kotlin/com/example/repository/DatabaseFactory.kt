0430f2967c475b984f960ea9b0cb30ab61f332c7
paulpriyam <ppaul2204!gmail.com>
2022-07-24 20:48:09 +0530

    added DATABASE_URL of Heroku postgres

--- src/main/kotlin/com/example/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/example/repository/DatabaseFactory.kt
@@ -9,6 +9,7 @@ import org.jetbrains.exposed.sql.Database
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
 
@@ -28,10 +29,16 @@ transaction {
     fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER") // 1
-        config.jdbcUrl = System.getenv("DATABASE_URL") // 2
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val username = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+
+        config.jdbcUrl =
+            "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
         config.validate()
 
         return HikariDataSource(config)
